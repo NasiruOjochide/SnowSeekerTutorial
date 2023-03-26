bf9798beea472ffed57fa61a6c8eda3298437722
Danjuma Nasiru <talk2danjumanas@gmail.com>
2023-03-26 10:29:23 +0100

    Initial Commit

---
+++ SnowSeekerTutorial/SnowSeekerTutorialApp.swift
@@ -0,0 +1,17 @@
+//
+//  SnowSeekerTutorialApp.swift
+//  SnowSeekerTutorial
+//
+//  Created by Danjuma Nasiru on 26/03/2023.
+//
+
+import SwiftUI
+
+@main
+struct SnowSeekerTutorialApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
